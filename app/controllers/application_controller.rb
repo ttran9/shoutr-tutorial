7ec29d90e3d9bde067d5f6dab0febb54346a0572
jstran9 <wptran58@gmail.com>
2019-09-04 21:49:15 -0700

    Fix user page when not signed in.
    Added Guest#liked?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+
+  protect_from_forgery with: :exception
+
+  def current_user
+    super || Guest.new
+  end
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,4 +2,8 @@ class Guest
 	def email
 		""	
 	end
+
+	def liked?(_) # we don't care about the parameter passed in
+		false
+	end
 end
