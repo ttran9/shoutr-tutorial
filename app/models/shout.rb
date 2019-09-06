1e9e5f61910d1b48a491ee11a8cf8b6d82ea537b
jstran9 <wptran58@gmail.com>
2019-09-05 22:16:02 -0700

    Refactor search.

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -6,4 +6,7 @@ class Shout < ApplicationRecord
 
   # delegate "makes it clear that this is a method that belongs to a relationship (between user and shout)"
   delegate :username, to: :user
+
+  scope :search, ->(term:) { joins("LEFT JOIN text_shouts ON content_type= 'TextShout' AND content_id = text_shouts.id").
+      where("text_shouts.body LIKE ?", "%#{term}%") }
 end
---
+++ app/services/search.rb
@@ -0,0 +1,11 @@
+class Search
+  attr_reader :term
+  def initialize(term:)
+    @term = term
+  end
+
+  def run
+    ShoutSearchQuery.new(term: "##{term}").to_relation
+  end
+  alias results run # so we can call "results" from the show view but invoke our run method above.
+end
