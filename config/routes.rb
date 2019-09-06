0e0b3f5b2794bfad005adc7a02446802d2adfa4d
jstran9 <wptran58@gmail.com>
2019-09-05 21:55:06 -0700

    Simple search example.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
       delete "unlike" => "likes#destroy"
     end
   end
+
+  resources :hashtags, only: [:show]
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
