66fd1e07a52bd268fdc0cb4f97c7b9ae2331ccab
jstran9 <wptran58@gmail.com>
2019-09-05 22:21:04 -0700

    Allowing this object to be more abstract and have more specific use cases.

--- app/queries/shout_search_query.rb
+++ app/queries/shout_search_query.rb
@@ -1,15 +1,15 @@
 class ShoutSearchQuery
-  def initialize(term:)
+  def initialize(term:, scope: Shout.all)
     @term = term
   end
 
   def to_relation
-    Shout.
+    scope.
         joins("LEFT JOIN text_shouts ON content_type= 'TextShout' AND content_id = text_shouts.id").
         where("text_shouts.body LIKE ?", "%#{term}%")
   end
 
   private
 
-  attr_reader :term
+  attr_reader :term, :scope
 end
