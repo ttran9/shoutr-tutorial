e02b14d7fb53d3acb2449dfb020e3edde1b4a39d
jstran9 <wptran58@gmail.com>
2019-09-06 22:44:54 -0700

    Refactor hashtag searching
    The hashtag symbol should be passed by the controller

--- app/queries/shout_search_query.rb
+++ app/queries/shout_search_query.rb
@@ -1,15 +1,15 @@
 class ShoutSearchQuery
-  def initialize(term:, scope: Shout.all)
+  def initialize(term:)
     @term = term
   end
 
   def to_relation
-    scope.
+    Shout.
         joins("LEFT JOIN text_shouts ON content_type= 'TextShout' AND content_id = text_shouts.id").
         where("text_shouts.body LIKE ?", "%#{term}%")
   end
 
   private
 
-  attr_reader :term, :scope
+  attr_reader :term
 end
--- app/services/search.rb
+++ app/services/search.rb
@@ -5,7 +5,7 @@ class Search
   end
 
   def run
-    ShoutSearchQuery.new(term: "##{term}").to_relation
+    ShoutSearchQuery.new(term: "#{term}").to_relation
   end
   alias results run # so we can call "results" from the show view but invoke our run method above.
 end
